d8ebfc4852604fdd5827f2e16a56f84b4d1def73
kriss57 <cgdamoniac@gmail.com>
2025-08-30 12:22:45 +0200

    css mod

--- src/app/(publicPages)/layout.js
+++ src/app/(publicPages)/layout.js
@@ -6,7 +6,6 @@ export default function Layout({ children }) {
         <>
             <Header />
             <main>
-                <p>Public Layout</p>
                 {children}
             </main>
             <Footer />
--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body >
-        <div>Root Layout</div>
+
         {children}
       </body>
     </html>
